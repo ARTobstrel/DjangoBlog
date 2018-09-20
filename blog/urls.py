8576c01cdc672213d8e7dea6bf10401f4177d399
Artem Fadeev <artemfap@yandex.ru>
2018-09-21 00:38:42 +0300

    commit 3 creation model

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', post_list, name='post_list')
+    path('', post_list, name='post_list'),
+    path('post/<str:slug>/', post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from blog.models import Post
 
 
 # Create your views here.
-
 def post_list(request):
-    a = request
-    return render(request, 'blog/index.html', {'request': a, 'req_dir': dir(request)})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', {'posts':posts})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__exact=slug) #точное соответствие slug
+    return render(request, 'blog/post_detail.html', {'post': post})
